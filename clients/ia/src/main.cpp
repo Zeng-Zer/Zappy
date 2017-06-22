12d3c055a889ab1786252f829cfce106d84e76e6
Jamy Bailly <jamy.bailly@epitech.eu>
2017-06-22 15:51:40 +0200

    [ia] add usage

--- clients/ia/src/main.cpp
+++ clients/ia/src/main.cpp
@@ -8,7 +8,18 @@
 ** Last update Thu Jun 22 15:32:00 2017 Jamy Bailly
 */
 
+#include <stdio.h>
+
+void	usage(void)
+{
+  puts("USAGE: ./zappy_ai -p port -n name -h machine");
+  puts("\tport    is the port number");
+  puts("\tname    is the name of the team");
+  puts("\tmachine is the name of the machine; localhost by default");
+}
+
 int	main(void)
 {
+  usage();
   return (0);
 }
