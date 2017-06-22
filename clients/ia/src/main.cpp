acef6cb7a8cf0d77319d7197e63c78bfabff465b
Jamy Bailly <jamy.bailly@epitech.eu>
2017-06-22 15:36:07 +0200

    [ia] start

---
+++ clients/ia/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** main.c for main in
+**
+** Made by Jamy Bailly
+** Login   <baillyj@epitech.net>
+**
+** Started on  Thu Jun 22 15:32:00 2017 Jamy Bailly
+** Last update Thu Jun 22 15:32:00 2017 Jamy Bailly
+*/
+
+int	main(void)
+{
+  return (0);
+}
