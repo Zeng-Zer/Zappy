6ce1b009b4675a64d6c36007510adceaa858b32e
Julien Andreani <julien.andreani@epitech.eu>
2017-07-02 21:49:53 +0000

    GRAPH => little fixes

--- clients/graphic/src/ImageHandler.cpp
+++ clients/graphic/src/ImageHandler.cpp
@@ -4,18 +4,20 @@
 std::unique_ptr<ImageHandler>	ImageHandler::_instance(nullptr);
 
 ImageHandler::ImageHandler() {
+  _textures = {
+    {PLAYER, {sf::Texture(), sf::Vector2i(14, 10)}},
+    {RESOURCE, {sf::Texture(), sf::Vector2i(15, 15)}},
+    {MAP, {sf::Texture(), sf::Vector2i(9, 9)}}
+  };
   if (!std::get<0>(_textures[PLAYER]).loadFromFile("./media/images/player.png"))
     throw(Error());
   std::get<0>(_textures[PLAYER]).setSmooth(true);
-  std::get<1>(_textures[PLAYER]) = sf::Vector2i(14, 10);
   if (!std::get<0>(_textures[RESOURCE]).loadFromFile("./media/images/ressource.png"))
     throw(Error());
   std::get<0>(_textures[RESOURCE]).setSmooth(true);
-  std::get<1>(_textures[RESOURCE]) = sf::Vector2i(15, 15);
   if (!std::get<0>(_textures[MAP]).loadFromFile("./media/images/map.png"))
     throw(Error());
   std::get<0>(_textures[MAP]).setSmooth(true);
-  std::get<1>(_textures[MAP]) = sf::Vector2i(9, 9);
 }
 
 ImageHandler::~ImageHandler() {}
